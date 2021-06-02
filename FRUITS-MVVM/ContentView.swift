d92ca13775cd5880c75c6bba4f272dfdf9268a2b
JJ Santos <jj@deltaprotect.co>
2021-06-02 10:13:02 -0500

    Initial Commit

---
+++ FRUITS-MVVM/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FRUITS-MVVM
+//
+//  Created by JJ Santos on 02/06/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FRUITS-MVVM/FRUITS_MVVMApp.swift
@@ -0,0 +1,17 @@
+//
+//  FRUITS_MVVMApp.swift
+//  FRUITS-MVVM
+//
+//  Created by JJ Santos on 02/06/21.
+//
+
+import SwiftUI
+
+@main
+struct FRUITS_MVVMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
